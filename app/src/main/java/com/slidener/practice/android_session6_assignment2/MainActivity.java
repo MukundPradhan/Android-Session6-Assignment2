744a05c26258d7feab43831a5c68520beca1b037
MukundPradhan <replyme.mmp@gmail.com>
2016-03-16 19:46:15 +0530

    Initial commit

---
+++ app/src/main/java/com/slidener/practice/android_session6_assignment2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.slidener.practice.android_session6_assignment2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
